cfc59c1b3c3f05e54e95cec2374609f1954fbfcc
unknown <dany4ac@gmail.com>
2022-09-09 23:28:32 -0500

    pom v2

--- src/test/java/runner/browser_manager/ChromeDriverManager.java
+++ src/test/java/runner/browser_manager/ChromeDriverManager.java
@@ -4,10 +4,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ChromeDriverManager extends DriverManager{
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 0a9a3396976f30d6e629efc059c6c2d647b0a932
     @Override
     public void createDriver(){
         System.setProperty("webdriver.chrome.driver", "./src/test/resources/chromedriver/chromedriver.exe");
--- src/test/java/runner/browser_manager/DriverType.java
+++ src/test/java/runner/browser_manager/DriverType.java
@@ -1,12 +1,6 @@
 package runner.browser_manager;
 
 public enum DriverType {
-
-<<<<<<< HEAD
         CHROME,
         OPERA,
-=======
-    CHROME,
-    OPERA,
->>>>>>> 0a9a3396976f30d6e629efc059c6c2d647b0a932
 }
